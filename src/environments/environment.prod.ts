2bb001fed014a990fe67330c5f0cdd51bca43cf8
Ahmed Abouemaira <a.aboemira1@gmail.com>
2022-10-05 00:29:56 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  APIURL:"http://18.168.226.166/:8080/",
-  AuthURL:"http://18.168.226.166/:9090/",
-  ResourceURL:"http://18.168.226.166/:8080/",
+  APIURL:"http://3.11.13.201:8080/",
+  AuthURL:"http://3.11.13.201:9090/",
+  ResourceURL:"http://3.11.13.201:8080/",
   clientID:"aabouemira",
   clientSecret:"9999",
-  redirectURI:"http://18.168.226.166/Login/debug"
+  redirectURI:"http://3.11.13.201/Login/debug"
 };
