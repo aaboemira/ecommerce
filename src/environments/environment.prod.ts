1349b82510fb9f06b6ca755418a86328456780af
Ahmed Abouemaira <a.aboemira1@gmail.com>
2022-10-04 20:22:40 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  APIURL:"http://localhost:8080/",
-  AuthURL:"http://localhost:9090/",
-  ResourceURL:"http://localhost:8080/",
+  APIURL:"http://18.168.226.166/:8080/",
+  AuthURL:"http://18.168.226.166/:9090/",
+  ResourceURL:"http://18.168.226.166/:8080/",
   clientID:"aabouemira",
   clientSecret:"9999",
-  redirectURI:"https://288b-41-68-73-255.eu.ngrok.io/Login/debug"
+  redirectURI:"http://18.168.226.166/Login/debug"
 };
